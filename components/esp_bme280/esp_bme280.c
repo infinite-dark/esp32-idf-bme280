b7ff5a1ca5af59fcf9084ff3f3f53dda31abe3b6
infinite-dark <infinite.dark.137@gmail.com>
2025-12-29 18:22:53 +0100

    Implement pressure compensation formula

--- components/esp_bme280/esp_bme280.c
+++ components/esp_bme280/esp_bme280.c
@@ -236,17 +236,50 @@ int32_t simple_test(const bme280_const_handle_t bme280_sensor) {
 
 }
 
-int32_t compensate_temperature(const bme280_const_handle_t sensor, const int32_t adc_T, int32_t * const T_fine) {
+int32_t compensate_temperature(const bme280_const_handle_t bme280_sensor, const int32_t adc_T, int32_t * const T_fine) {
 
     int32_t var1, var2, T;
 
-    var1 = (((adc_T >> 3) - ((int32_t)sensor->tp_calib.calib.dig_T1 << 1)) * ((int32_t)sensor->tp_calib.calib.dig_T2)) >> 11;
-    var2 = (((((adc_T >> 4) - ((int32_t)sensor->tp_calib.calib.dig_T1)) * ((adc_T >> 4) - ((int32_t)sensor->tp_calib.calib.dig_T1))) >> 12)
-        * ((int32_t)sensor->tp_calib.calib.dig_T3)) >> 14;
+    var1 = (((adc_T >> 3) - ((int32_t)bme280_sensor->tp_calib.calib.dig_T1 << 1)) * ((int32_t)bme280_sensor->tp_calib.calib.dig_T2)) >> 11;
+    var2 = (((((adc_T >> 4) - ((int32_t)bme280_sensor->tp_calib.calib.dig_T1)) * ((adc_T >> 4) - ((int32_t)bme280_sensor->tp_calib.calib.dig_T1))) >> 12)
+        * ((int32_t)bme280_sensor->tp_calib.calib.dig_T3)) >> 14;
 
     *T_fine = var1 + var2;
     T = (*T_fine * 5 + 128) >> 8;
 
     return T;
 
+}
+
+uint32_t compensate_pressure(const bme280_const_handle_t bme280_sensor, const int32_t adc_P, const int32_t T_fine) {
+
+    int32_t var1, var2;
+    uint32_t P;
+    
+    var1 = (((int32_t)T_fine) >> 1) - (int32_t)64000;
+    var2 = (((var1 >> 2) * (var1 >> 2)) >> 11 ) * ((int32_t)bme280_sensor->tp_calib.calib.dig_P6);
+    var2 = var2 + ((var1 * ((int32_t)bme280_sensor->tp_calib.calib.dig_P5)) << 1);
+    var2 = (var2 >> 2) + (((int32_t)bme280_sensor->tp_calib.calib.dig_P4) << 16);
+    var1 = (((bme280_sensor->tp_calib.calib.dig_P3 * (((var1 >> 2) * (var1 >> 2)) >> 13 )) >> 3) + ((((int32_t)bme280_sensor->tp_calib.calib.dig_P2) * var1) >> 1)) >> 18;
+    var1 = ((((32768 + var1)) * ((int32_t)bme280_sensor->tp_calib.calib.dig_P1)) >> 15);
+
+    if (var1 == 0) {
+        return 0;
+    }
+
+    P = (((uint32_t)(((int32_t)1048576) - adc_P) - (var2 >> 12))) * 3125;
+
+    if (P < 0x80000000) {
+        P = (P << 1) / ((uint32_t)var1);
+    }
+    else {
+        P = (P / (uint32_t)var1) * 2;
+    }
+
+    var1 = (((int32_t)bme280_sensor->tp_calib.calib.dig_P9) * ((int32_t)(((P >> 3) * (P >> 3)) >> 13))) >> 12;
+    var2 = (((int32_t)(P >> 2)) * ((int32_t)bme280_sensor->tp_calib.calib.dig_P8)) >> 13;
+    P = (uint32_t)((int32_t)P + ((var1 + var2 + bme280_sensor->tp_calib.calib.dig_P7) >> 4));
+
+    return P;
+    
 }
